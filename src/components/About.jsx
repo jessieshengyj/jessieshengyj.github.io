44b4a9fbd129d1eaaa5bbcd2b6583da4839ad2e6
Jessie Sheng <jessieshengyj@gmail.com>
2025-02-20 22:41:29 -0800

    basic layout for about and experience

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,29 @@ import "./About.scss";
 
 function About() {
     return (
-        <div className="about">
-            <h2>About Me</h2>
-            <p>Hi, I'm Jessie.</p>
+        <div className="about flex justify-center px-4">
+            <div className="chat chat-start w-full max-w-lg md:max-w-1/2">
+                <div className="chat-image avatar avatar-online">
+                    <div className="w-32 h-32 rounded-full">
+                        <img
+                            alt="Jessie's Avatar"
+                            src="/my-pfp.jpg"
+                        />
+                    </div>
+                </div>
+                <div className="chat-header">
+                    Jessie Sheng
+                    <time className="text-xs opacity-50">Now</time>
+                </div>
+                <div className="chat-bubble py-5 px-5">
+                    Hello! I'm Jessie, a 4th-year Computer Science student at the University of British Columbia, minoring in Data Science. I'm
+                    passionate about software engineering, front-end development, and data-driven problem-solving. 🌱
+                </div>
+                <div className="chat-footer opacity-50">Delivered</div>
+            </div>
         </div>
     );
 }
 
 export default About;
+
